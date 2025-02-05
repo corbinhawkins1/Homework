46c2e163c416c9b6236dd9300623695c378b54f8
Corbin Hawkins <corbinhawkins1@byui.edu>
2025-02-04 17:48:31 -0700

    final2

--- prove/Develop02/Entries.cs
+++ prove/Develop02/Entries.cs
@@ -3,20 +3,21 @@ public class Entries
 public string response;
 public string prompt;
 public string DateTime;
-public Entries(string response, string datetime, string prompt)
+public string mood;
+public Entries(string response, string datetime, string prompt, string mood)
 {
 this.DateTime=datetime;
 this.prompt=prompt;
 this.response=response;
-
+this.mood=mood;
 
 }
 public string display(){
-return $"{response}{prompt}{DateTime}";
+return $"{response}{prompt}{DateTime}{mood}";
 
 
 }
 public string export(){
-return $"{response}|{prompt}|{DateTime}";
+return $"{response}|{prompt}|{DateTime}|{mood}";
 }
 }
--- prove/Develop02/journal.cs
+++ prove/Develop02/journal.cs
@@ -20,15 +20,17 @@ class Journal
     {
         Random random = new Random();
         string prompt = prompts[random.Next(prompts.Count)];
-
+        Console.WriteLine("What was your mood today?: ");
+        string mood = Console.ReadLine();
         Console.WriteLine("\n--- Journal Prompt ---");
         Console.WriteLine(prompt);
         Console.Write("Your response: ");
         string response = Console.ReadLine();
         DateTime theCurrentTime = DateTime.Now;
         string dateText = theCurrentTime.ToShortDateString();
-        Entries entry = new Entries(response, dateText, prompt);
+        Entries entry = new Entries(response, dateText, prompt, mood);
         entries.Add(entry);
+
     }
 
     public void DisplayEntries()
@@ -81,7 +83,8 @@ foreach (string line in lines)
     string response = parts[0];
     string prompt = parts[1];
     string date = parts[2];
-    Entries entry = new Entries(response, date, prompt);
+    string mood = parts[3];
+    Entries entry = new Entries(response, date, prompt, mood);
         entries.Add(entry);
 
 }
